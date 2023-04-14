ed911bb9c6306862d78c173e20a81b33612f5387
Iman071 <52603994+Iman071@users.noreply.github.com>
2023-04-14 20:43:04 +0430

    data generation

--- Dataset/merger.py
+++ Dataset/merger.py
@@ -4,13 +4,12 @@ xl= pd.ExcelFile('Dataset_HEAs15.xlsx')
 dfs = [xl.parse(sheet_name) for sheet_name in xl.sheet_names]
 df = pd.concat(dfs, ignore_index=True)
 #%%
-df = df.drop(df[df.Phase == 'multi-phase'].index)
 cat = df['Phase'].value_counts()
 to_keep  = cat.index[:12]
 df = df[df['Phase'].isin(to_keep)]
-df.to_csv('1026_2_noP.csv', index=False)
+df.to_csv('11295_2_noP.csv', index=False)
 #%%
 cat = df['Phase'].value_counts()
 
 df = df.drop_duplicates()
-df.to_csv('8054_2_noP.csv', index=False)
+df.to_csv('8504_2_noP.csv', index=False)
