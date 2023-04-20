7c43d5a7f43f78bb1bea19e9a63f944c779b6c29
Iman071 <52603994+Iman071@users.noreply.github.com>
2023-04-20 10:49:53 +0430

    hea

--- Visulize/elements analysis.py
+++ Visulize/elements analysis.py
@@ -2,22 +2,22 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 #%%
-data = pd.read_csv('dataset10387_70.csv')
-data.drop(data.columns[0], axis=1, inplace=True)
-#%%
-el_num = data['Num_el'].value_counts()
-#%%
-# data = data.set_index('index')
-output = data['Phase'].to_frame()
-data.drop(data.columns[-2], axis=1, inplace=True)
-data.drop(data.columns[1], axis=1, inplace=True)
-#%%
-data = data.iloc[:,:-14]
-df = pd.concat([data, output], axis=1)
-df.drop(df.columns[0], axis=1, inplace=True)
-cat_num = df['Phase'].value_counts()
+df = pd.read_csv('6676_70_noP.csv')
 
-df.drop_duplicates(keep='first', inplace=True)
+# data = data.set_index('index')
+output = df['Phase'].to_frame()
+df.drop(df.columns[-1], axis=1, inplace=True) # drop phase
+# df.drop(data.columns[1], axis=1, inplace=True)
+# df.drop(data.columns[1], axis=1, inplace=True)
+#%%
+df = df.iloc[:,:-14]
+df = pd.concat([df, output], axis=1)
+
+
+#%%
+cat_num = df['Phase'].value_counts()
+el_num = df['Num_el'].value_counts()
+df = df.iloc[:, 2:]
 #%%
 def phase_separation(df):
     BCC = df[df['Phase'] == 'BCC']
@@ -26,31 +26,18 @@ def phase_separation(df):
     AM = df[df['Phase'] == 'AM']
     IM = df[df['Phase'] == 'IM']
     HCP= df[df['Phase'] == 'HCP']
-    # FCC_IM = df[df['Phase'] == 'FCC + Im']
-    # BCC_IM = df[df['Phase'] == 'BCC + Im']
+    FCC_IM = df[df['Phase'] == 'FCC+IM']
+    BCC_IM = df[df['Phase'] == 'BCC+IM']
     
-    
-    # FCC_des = FCC.describe()
-    # AM_des = AM.describe()
-    
-    # BCC_des = BCC.describe()
-    # IM_des = IM.describe()
-    # FCC_IM_des = FCC_IM.describe()
-    # BCC_IM_des = BCC_IM.describe()
-    # FCC_BCC_des = FCC_BCC.describe()
-    
-    return BCC, FCC, HCP, BCC_FCC, AM, IM
+    return BCC, FCC, HCP, BCC_FCC, AM, IM, BCC_IM, FCC_IM
 
 def remove_categorical(df):#Find and remove all categorical columns
-    # find all columns in the dataframe
     columns = df.columns
-    # check if each column is categorical
     categorical = [col for col in columns if df[col].dtype == "object"]
-    # drop the categorical columns from the dataframe
     df = df.drop(categorical, axis=1)
     return df
 #%%
-BCC, FCC, HCP, BCC_FCC, AM, IM = phase_separation(df)
+BCC, FCC, HCP, BCC_FCC, AM, IM, BCC_IM, FCC_IM = phase_separation(df)
 #%% FCC versus BCC
 
 plt.rcParams["font.family"] = "Times New Roman"
@@ -74,49 +61,39 @@ ax.bar(top_10_BCC.index, top_10_BCC, width=0.4,
     
 ax.bar(top_10_FCC.index, top_10_FCC, width=-0.4,
        align='edge', label='FCC', color = 'r')
-ax.set_ylabel('Compositon (mean Value)',)
+ax.set_ylabel('Compositon (mean value)',)
 # ax.set_title('Presence of elements in BCC and FCC')
 ax.legend()
 
 fig.savefig('comp_el(FCC_BCC).jpg', dpi=600)
 
-#%%
+#%% Intermetallic vs Amorphous
 
 plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams.update({'font.size': 20})
-BCC = remove_categorical(BCC)
-BCC_ = BCC.describe()
-FCC = remove_categorical(FCC)
-FCC_ = FCC.describe()
 IM = remove_categorical(IM)
 IM_ = IM.describe()
+AM = remove_categorical(AM)
+AM_ = AM.describe()
 
-top_10_BCC = BCC_.loc['mean'].nlargest(11)
-top_10_FCC = FCC_.loc['mean'].nlargest(11)
-top_10_IM = IM_ .loc['mean'].nlargest(11)
 
-fig, ax = plt.subplots(figsize=(12, 8))
-# col_names_row1 = ['Cr', 'Al']
-# top_10_BCC.index = col_names_row1
-# top_10_FCC.index = col_names_row1
-ax.bar(top_10_BCC.index, top_10_BCC, width=0.4,
-       align='edge', label='BCC', color='b')
+top_10_IM = IM_.loc['mean'].nlargest(12)
+top_10_AM = AM_.loc['mean'].nlargest(12)
+
+
+fig2, ax2 = plt.subplots(figsize=(12, 8))
+
+ax2.bar(top_10_IM.index, top_10_IM, width=0.4,
+       align='edge', label='Intermetallic', color='black')
     
-ax.bar(top_10_FCC.index, top_10_FCC, width=-0.4,
-       align='edge', label='FCC', color = 'r')
+ax2.bar(top_10_AM.index, top_10_AM, width=-0.4,
+       align='edge', label='Amorphous', color = 'y')
 
-ax.bar(top_10_IM.index, top_10_IM, width=-0.8,
-       align='edge', label='IM', color = 'black')
+ax2.set_ylabel('Compositon (mean value)',)
 
+ax2.legend()
 
-ax.set_ylabel('Compositon (mean Value)',)
-# ax.set_title('Presence of elements in BCC and FCC')
-ax.legend()
-
-fig.savefig('comp_el(FCC_BCC_IM).jpg', dpi=600)
-
-
-
+fig2.savefig('comp_el(AM_IM).jpg', dpi=600)
 
 #%%
 def plot (xp, xpn):
@@ -130,10 +107,10 @@ def plot (xp, xpn):
     axe.set_title(f'{name}')
     
 #%%
+plot(FCC_IM, "FI")
+
+plot(BCC_IM, "FI")
 plot(IM, "IM")
-
-
-
 #%%
 BCC = remove_categorical(BCC)
 BCC_ = BCC.describe()
